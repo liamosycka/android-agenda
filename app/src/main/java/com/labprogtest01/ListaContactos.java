aaddf94e5231cebbdf916e4066bd692a4f423f35
liamosycka <osyckaliam@gmail.com>
2019-10-10 18:11:36 -0300

    estructurando listaContactos

--- app/src/main/java/com/labprogtest01/ListaContactos.java
+++ app/src/main/java/com/labprogtest01/ListaContactos.java
@@ -1,10 +1,10 @@
 package com.labprogtest01;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 
 public class ListaContactos extends AppCompatActivity {
@@ -13,9 +13,8 @@ public class ListaContactos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_contactos);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+    }
+    public void pressButonInfo(View view) {
 
     }
 }
